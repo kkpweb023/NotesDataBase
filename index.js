22881fbf868ed79353ba80443d151ffd5909f89d
kkpweb023 <kpandey023@gmail.com>
2023-04-30 19:08:35 +0530

    new url back

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(cors())
+
+
+/*
 app.use(cors({
 
        origin: "https://kkpweb023.github.io" || "http://localhost:3000"
@@ -12,6 +16,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
+  */
 
 const port = process.env.PORT || 4000 ;
 require('./DataBase/config');
