a6ca4f1812b42a641c7b037781bf47333fa470e0
kkpweb023 <kpandey023@gmail.com>
2023-04-30 16:25:18 +0530

    cors origin add

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+app.use(cors({
+
+       origin: "https://kkpweb023.github.io" || "http://localhost:3000"
+}));
 const port = process.env.PORT || 4000 ;
 require('./DataBase/config');
 let bodyParser = require('body-parser');
